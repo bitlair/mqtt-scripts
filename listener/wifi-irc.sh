5440fa922582ea2e9a7bf463cbbbeca8bf2085ab
polyfloyd <floyd@polyfloyd.net>
2016-09-29 03:42:41 +0200

    Add script for wifi to IRC notices

---
+++ listener/wifi-irc.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+mqtt-simple -h mqtt.bitlair.nl -s "bitlair/wifi/+" |
+    while read event; do
+        ssid=$(  echo "$event" | cut -d ' ' -f 1 | cut -d '/' -f 3)
+        action=$(echo "$event" | cut -d ' ' -f 2)
+        mac=$(   echo "$event" | cut -d ' ' -f 3)
+        signal=$(echo "$event" | cut -d ' ' -f 4)
+
+        dn=$(lookup-displayname "$mac")
+        if [ "$dn" == "{}" ]; then
+            continue
+        fi
+        displayname=$(echo "$dn" | jq -r '.displayname')
+        device=$(echo "$dn" | jq -r '.device')
+
+        if [ "$action" == "join" ]; then
+            on="on"
+            if [ "$signal" != "" ]; then
+                sigstr="(signal: $signal dB SNR)"
+            fi
+        elif [ "$action" == "part" ]; then
+            on="from"
+            sigstr=""
+        else
+            continue
+        fi
+
+        irc-say "$displayname ${action}s with their $device $on $ssid $sigstr"
+    done
