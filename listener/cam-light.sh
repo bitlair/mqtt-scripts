d4cb308be8cb5e6ee08268bab65d98034377dfd7
polyfloyd <floyd@polyfloyd.net>
2017-01-13 15:13:18 +0100

    Fix the camview lights going nuts when more people start watching

--- listener/cam-light.sh
+++ listener/cam-light.sh
@@ -8,7 +8,7 @@ set -o pipefail
 light_pid=""
 mqtt-simple --message-only -h mqtt.bitlair.nl -s "bitlair/cam/$CAM_NAME" |
     while read num; do
-        if [ "$num" -gt 0 ]; then
+        if [[ "$num" -gt 0 && -z "$light_pid" ]]; then
             perl -e 'for (0..12) { print "\x00\x00\x00" x 150 }' > /tmp/camera
             perl -e 'for (0..12) { print "\x00\xff\x00" x 150 }' > /tmp/camera
             perl -e 'for (0..12) { print "\x00\x00\x00" x 150 }' > /tmp/camera
@@ -16,5 +16,6 @@ mqtt-simple --message-only -h mqtt.bitlair.nl -s "bitlair/cam/$CAM_NAME" |
             light_pid=$!
         elif [ ! -z "$light_pid" ]; then
             kill "$light_pid"
+            light_pid=""
         fi
     done
