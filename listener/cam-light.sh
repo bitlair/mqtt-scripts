1c00244a3aee2009d2eede2b72fd720c3552bd80
polyfloyd <floyd@polyfloyd.net>
2017-01-25 13:57:08 +0100

    Fix the camera indicator not working when more people start watching

--- listener/cam-light.sh
+++ listener/cam-light.sh
@@ -8,12 +8,14 @@ set -o pipefail
 light_pid=""
 mqtt-simple --message-only -h mqtt.bitlair.nl -s "bitlair/cam/$CAM_NAME" |
     while read num; do
-        if [[ "$num" -gt 0 && -z "$light_pid" ]]; then
-            perl -e 'for (0..12) { print "\x00\x00\x00" x 150 }' > /tmp/leds-camera
-            perl -e 'for (0..12) { print "\xff\x00\x00" x 150 }' > /tmp/leds-camera
-            perl -e 'for (0..12) { print "\x00\x00\x00" x 150 }' > /tmp/leds-camera
-            perl -e 'for (;;)    { print "\xff\x00\x00" x 150 }' > /tmp/leds-camera &
-            light_pid=$!
+        if [[ "$num" -gt 0 ]]; then
+            if [[ -z "$light_pid" ]]; then
+                perl -e 'for (0..12) { print "\x00\x00\x00" x 150 }' > /tmp/leds-camera
+                perl -e 'for (0..12) { print "\xff\x00\x00" x 150 }' > /tmp/leds-camera
+                perl -e 'for (0..12) { print "\x00\x00\x00" x 150 }' > /tmp/leds-camera
+                perl -e 'for (;;)    { print "\xff\x00\x00" x 150 }' > /tmp/leds-camera &
+                light_pid=$!
+            fi
         elif [ ! -z "$light_pid" ]; then
             kill "$light_pid"
             light_pid=""
