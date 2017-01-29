8d9165bd7a9ddcea3552306a1d52cd53099dcd1b
polyfloyd <floyd@polyfloyd.net>
2017-01-29 13:56:30 +0100

    Don't try to clear retained messages

--- listener/doorbell-light.sh
+++ listener/doorbell-light.sh
@@ -4,8 +4,6 @@ set -eu
 set -o pipefail
 
 while true; do
-    # Clear retained messages if any.
-    mqtt-simple -r -h mqtt.bitlair.nl -p "bitlair/doorbell" -m ''
     sleep 1
     mqtt-simple --one -h mqtt.bitlair.nl -s "bitlair/doorbell" > /dev/null
     for i in {0..5}; do
