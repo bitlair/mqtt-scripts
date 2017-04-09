a1a16ad56af16b59571c7973e1ff8ab47623c291
polyfloyd <floyd@polyfloyd.net>
2017-04-09 16:17:37 +0200

    listener/flash: Clear retained messages

--- listener/flash-light.sh
+++ listener/flash-light.sh
@@ -5,7 +5,11 @@ set -o pipefail
 
 while true; do
     sleep 1
-    mqtt-simple --one -h mqtt.bitlair.nl -s "bitlair/flash" > /dev/null
+    mqtt-simple -r -h mqtt.bitlair.nl -p bitlair/flash -m ''
+    m=$(mqtt-simple --one -h mqtt.bitlair.nl -s "bitlair/flash")
+    if [[ -z $m ]]; then
+        continue
+    fi
     for i in {0..2}; do
         perl -e 'for (0..3) { print "\xff\x00\x00" x 150 }' > /tmp/leds-messages
         perl -e 'for (0..3) { print "\x00\x00\xff" x 150 }' > /tmp/leds-messages
